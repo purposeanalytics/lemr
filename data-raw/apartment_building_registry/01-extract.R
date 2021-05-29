cefce11241b9793f541e93ceca15c61b5423ff6f
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-28 20:53:51 -0400

    Add note for future on getting datasets that have changed / on keeping track of date retrieved

--- data-raw/apartment_building_registry/01-extract.R
+++ data-raw/apartment_building_registry/01-extract.R
@@ -5,6 +5,9 @@ library(dplyr)
 library(janitor)
 library(readr)
 
+# Potential TODO later on: look at the date this data set was last modified, and only download it if it is *after* the latest date we grabbed it?
+# Might be easier to keep track of date grabbed in a CSV rather than relying solely on the filenames (which are slightly more annoying to parse)
+
 # Get Apartment Building Registration Resource ----
 apartment_building_registry <- list_package_resources("https://open.toronto.ca/dataset/apartment-building-registration/") %>%
   filter(name == "Apartment Building Registration Data") %>%
