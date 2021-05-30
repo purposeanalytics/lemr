c1db0d08e2db47f7f97bfc69345f47919f727b5c
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-30 12:52:57 -0400

    Add geocoding details to apartment building registry documentation

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
 #' Apartment building registry
 #'
-#' Apartment building registry from City of Toronto, retrieved from \link{https://open.toronto.ca/dataset/apartment-building-registration/} on May 6, 2021. Contains the address of all rental apartment buildings in the city with 3 or more storeys and 10 or more units.
+#' Apartment building registry from City of Toronto, retrieved from \link{https://open.toronto.ca/dataset/apartment-building-registration/} and geocoded using the Bing geocoding API (\link{https://docs.microsoft.com/en-us/bingmaps/rest-services/locations/find-a-location-by-address}). Contains the address of all rental apartment buildings in the city with 3 or more storeys and 10 or more units.
 "apartment_building_registry"
