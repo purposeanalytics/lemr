15838a2e6b5966931d95f4e76d9686e773664961
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-02 17:13:43 -0400

    Add search bar for neighbourhood

--- R/app_server.R
+++ R/app_server.R
@@ -6,5 +6,6 @@
 #' @noRd
 app_server <- function(input, output, session) {
   address <- mod_address_search_server("address")
+  neighbourhood <- mod_neighbourhood_search_server("neighbourhood")
   mod_map_server("map", address)
 }
