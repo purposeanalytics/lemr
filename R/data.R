88dcba80737b493cdf291155722e40b02932f817
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-08 11:56:26 -0400

    Include examples using each data set, with sf loaded - required for them to load properly

--- R/data.R
+++ R/data.R
@@ -1,13 +1,22 @@
 #' Apartment building registry
 #'
-#' Apartment building registry from City of Toronto, retrieved from \link{https://open.toronto.ca/dataset/apartment-building-registration/} and geocoded using the Bing geocoding API (\link{https://docs.microsoft.com/en-us/bingmaps/rest-services/locations/find-a-location-by-address}). Contains the address of all rental apartment buildings in the city with 3 or more storeys and 10 or more units.
+#' Apartment building registry from City of Toronto, retrieved from \link{https://open.toronto.ca/dataset/apartment-building-registration/} and geocoded using the Bing geocoding API (\link{https://docs.microsoft.com/en-us/bingmaps/rest-services/locations/find-a-location-by-address}). Contains the address of all rental apartment buildings in the city with 3 or more storeys and 10 or more units.#'
+#' @examples
+#' library(sf)
+#' apartment_building_registry
 "apartment_building_registry"
 
 #' City of Toronto boundaries
 #'
 #' City of Toronto boundaries, retrieved from \link{https://open.toronto.ca/dataset/regional-municipal-boundary/}
+#' @examples
+#' library(sf)
+#' toronto
 "toronto"
 
 #' City of Toronto Neighbourhoods
 #' Neighbourhood boundaries from the City of Toronto, retrieved from \link{https://open.toronto.ca/dataset/neighbourhoods/}
+#' @examples
+#' library(sf)
+#' neighbourhoods
 "neighbourhoods"
