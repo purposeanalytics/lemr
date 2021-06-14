6206342ce7f76a0d8a36e5e121b13823b3c610f1
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-14 16:30:44 -0400

    Return address neighbourhood from module too

--- R/mod_address_search.R
+++ R/mod_address_search.R
@@ -21,12 +21,27 @@ mod_address_search_ui <- function(id) {
 #' Address Search Server Functions
 #'
 #' @noRd
-mod_address_search_server <- function(id) {
+mod_address_search_server <- function(id, search_method) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # For now, just return the address to be used by other modules
-    shiny::reactive(input$address)
+    # Update that the search method was via address
+    shiny::observeEvent(input$address, {
+      search_method("address")
+    })
+
+    # Get neighbourhood of address
+    neighbourhood <- shiny::reactive({
+      lemur::apartment_building_registry %>%
+        dplyr::filter(.data$bing_address == input$address) %>%
+        dplyr::pull(.data$neighbourhood)
+    })
+
+    # For now, just return the address and its neighbourhood to be used by other modules
+    list(
+      address = shiny::reactive(input$address),
+      neighbourhood = shiny::reactive(neighbourhood())
+    )
   })
 }
 
