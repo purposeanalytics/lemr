22494fa3eb74af608a9118ba3df1f1caf06aaa88
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-14 11:30:45 -0400

    Highlight, then zoom

--- R/zoom_map_to_address.R
+++ R/zoom_map_to_address.R
@@ -20,8 +20,8 @@ zoom_map_to_address <- function(map, address) {
   }
 
   map %>%
-    # Zoom to the address
-    mapboxer::fit_bounds(sf::st_bbox(searched_address), maxZoom = 15, pitch = 0, bearing = -15) %>%
     # Filter the "apartment_building_searched" layer to be for this address, so that that point is highlighted in red
-    mapboxer::set_filter(layer_id = "apartment_building_searched", list("==", "bing_address", address))
+    mapboxer::set_filter(layer_id = "apartment_building_searched", list("==", "bing_address", address)) %>%
+    # Zoom to the address
+    mapboxer::fit_bounds(sf::st_bbox(searched_address), maxZoom = 15, pitch = 0, bearing = -15)
 }
