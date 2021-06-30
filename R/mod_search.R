6dae59798efeee91d9af0501f69fc97b7230c646
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-30 15:53:11 -0400

    Use autocomplete and db for addresses

--- R/mod_search.R
+++ R/mod_search.R
@@ -12,27 +12,24 @@ mod_search_ui <- function(id) {
   shiny::tagList(
     shiny::column(
       width = 6,
-      shinyWidgets::pickerInput(
-        inputId = ns("address"),
+      dqshiny::autocomplete_input(
+        id = ns("address"),
         label = "Address",
-        choices = lemur::apartment_building_registry[["bing_address"]],
-        multiple = TRUE,
-        selected = NULL,
-        options = shinyWidgets::pickerOptions(liveSearch = TRUE, maxOptions = 1, size = 10)
+        placeholder = "Search address...",
+        options = address_points_just_address,
+        max_options = 10,
+        contains = TRUE
       )
     ),
     shiny::column(
       width = 6,
-      shiny::selectizeInput(
-        inputId = ns("neighbourhood"),
+      dqshiny::autocomplete_input(
+        id = ns("neighbourhood"),
         label = "Neighbourhood",
-        choices = sort(lemur::neighbourhoods[["neighbourhood"]]),
-        multiple = TRUE,
-        selected = NULL,
-        options = list(
-          placeholder = "Search neighbourhood...",
-          maxItems = 1
-        )
+        placeholder = "Search neighbourhood...",
+        options = sort(lemur::neighbourhoods[["neighbourhood"]]),
+        max_options = 10,
+        contains = TRUE
       )
     )
   )
@@ -41,34 +38,44 @@ mod_search_ui <- function(id) {
 #' search Server Functions
 #'
 #' @noRd
-mod_search_server <- function(id, address_and_neighbourhood, search_method) {
+mod_search_server <- function(id, lemur_db, address_and_neighbourhood, search_method) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
     # If address is selected, store address and neighbourhood
-    shiny::observeEvent(input$address, {
+    shiny::observeEvent(input$address, ignoreInit = TRUE, {
+      shiny::req(input$address != "")
+
       address_and_neighbourhood$address <- input$address
 
-      address_and_neighbourhood$neighbourhood <- lemur::apartment_building_registry %>%
-        dplyr::filter(.data$bing_address == input$address) %>%
-        dplyr::pull(.data$neighbourhood)
+      # Get neighbourhood of address
+      address_point <- address_points() %>%
+        dplyr::filter(.data$address == local(input$address)) %>%
+        dplyr::collect()
+
+      address_and_neighbourhood$neighbourhood <- address_point %>%
+        sf::st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+        sf::st_intersection(lemur::neighbourhoods) %>%
+        dplyr::pull(neighbourhood)
 
       # Update search method
       search_method("address")
 
       # Deselect neighbourhood
-      shiny::updateSelectizeInput(session = session, inputId = "neighbourhood", selected = "")
+      dqshiny::update_autocomplete_input(session = session, id = "neighbourhood", value = "")
     })
 
     # If neighbourhood is selected, store neighbourhood
-    shiny::observeEvent(input$neighbourhood, {
+    shiny::observeEvent(input$neighbourhood, ignoreInit = TRUE, {
+      shiny::req(input$neighbourhood != "")
+
       address_and_neighbourhood$neighbourhood <- input$neighbourhood
 
       # Update search method
       search_method("neighbourhood")
 
       # Deselect address
-      shinyWidgets::updatePickerInput(session = session, inputId = "address", selected = "")
+      dqshiny::update_autocomplete_input(session = session, id = "address", value = "")
     })
 
     # If the address_and_neighbourhood are cleared, reset both
@@ -76,11 +83,13 @@ mod_search_server <- function(id, address_and_neighbourhood, search_method) {
       {
         address_and_neighbourhood$address
         address_and_neighbourhood$neighbourhood
-      }, ignoreNULL = FALSE,
+      },
+      ignoreNULL = FALSE,
+      ignoreInit = TRUE,
       {
         if (is.null(address_and_neighbourhood$address) & is.null(address_and_neighbourhood$neighbourhood)) {
-          shiny::updateSelectizeInput(session = session, inputId = "neighbourhood", selected = "")
-        shinyWidgets::updatePickerInput(session = session, inputId = "address", selected = "")
+          dqshiny::update_autocomplete_input(session = session, id = "neighbourhood", value = "")
+          dqshiny::update_autocomplete_input(session = session, id = "address", value = "")
         }
       }
     )
