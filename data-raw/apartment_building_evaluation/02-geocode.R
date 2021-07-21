6a1efd40dfc4e2d6dc86d506ffd2710dcca44f73
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-07-21 15:46:59 -0400

    Start pipeline for apartment building evaluation

---
+++ data-raw/apartment_building_evaluation/02-geocode.R
@@ -0,0 +1,28 @@
+# Geocode apartment building evaluation
+# The data set can be connected to lemur::apartment_building_evaluation, so don't actually need to geocode - yay!
+
+library(dplyr)
+library(sf)
+devtools::load_all() # Load package itself to get geocode_address and read_latest_file
+
+# Read in latest extract
+apartment_building_evaluation <- read_latest_file(directory = here::here("data-raw", "apartment_building_evaluation", "extract"), suffix = "-apartment_building_evaluation.rds", fileext = "rds")
+
+apartment_building_evaluation <- apartment_building_evaluation %>%
+  left_join(apartment_building_registry %>%
+              select(rsn, starts_with("bing"), neighbourhood, geometry),
+            by = "rsn") %>%
+  st_sf()
+
+# Check that all apartments have geocoding
+apartments_need_geocoding <- apartment_building_evaluation %>%
+  filter(is.na(bing_address)) %>%
+  nrow()
+
+if (apartments_need_geocoding == 0) {
+  usethis::ui_done("All apartments have geocoding!")
+} else {
+  usethis::ui_todo("Not all apartments were in apartment_building_registry! {apartments_need_geocoding} need additional geocoding.")
+}
+
+# Save geocoded
