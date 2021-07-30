8563b1215e6d33cae36bca255d385099b4053fa9
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-07-30 12:41:18 -0400

    Change styling of amenity density

--- R/add_blank_amenity_density_layer.R
+++ R/add_blank_amenity_density_layer.R
@@ -25,8 +25,8 @@ add_blank_amenity_density_layer <- function(map) {
 
   map %>%
     # Add the layers (one for the fill, one for the outline)
-    mapboxer::add_fill_layer(source = mapboxer::as_mapbox_source(amenity_density_with_colours), fill_color = c("get", "colour"), id = "amenity_density_fill") %>%
-    mapboxer::add_line_layer(source = mapboxer::as_mapbox_source(amenity_density_with_colours), line_color = "black", line_width = 0.25, id = "amenity_density_outline") %>%
+    mapboxer::add_fill_layer(source = mapboxer::as_mapbox_source(amenity_density_with_colours), fill_color = c("get", "colour"), fill_opacity = 0.75, id = "amenity_density_fill") %>%
+    mapboxer::add_line_layer(source = mapboxer::as_mapbox_source(amenity_density_with_colours), line_color = "black", line_width = 0.5, id = "amenity_density_outline") %>%
   # Set the visibility to "none", so they're not shown
   mapboxer::set_layout_property(layer_id = "amenity_density_fill", "visibility", "none") %>%
     mapboxer::set_layout_property(layer_id = "amenity_density_outline", "visibility", "none")
