0015bfe80eb74f2db15d79983e17b423c00787df
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-08-10 11:40:06 -0400

    Use buttons instead of checks for layers

--- R/mod_layers.R
+++ R/mod_layers.R
@@ -12,18 +12,27 @@ mod_layers_ui <- function(id) {
     shiny::h1("Data"),
     shiny::column(
       width = 6,
-      shiny::checkboxGroupInput(
-        inputId = ns("point_layers"),
-        label = "Data points",
-        choices = point_layers_choices
+      bigger_padded("Point data"),
+      style = "padding-left: 0px;",
+      shinyWidgets::checkboxGroupButtons(
+        inputId = ns("apartment_buildings"),
+        choices = list("Apartment buildings" = "apartment_buildings"),
+        justified = TRUE
       ),
+      shinyWidgets::checkboxGroupButtons(
+        inputId = ns("apartment_evaluation"),
+        choices = list("RentSafeTO Evaluation Scores" = "apartment_evaluation"),
+        justified = TRUE
+      )
     ),
     shiny::column(
       width = 6,
-      shiny::checkboxGroupInput(
-        inputId = ns("aggregate_layers"),
-        label = "Aggregated data",
-        choices = aggregate_layers_choices
+      bigger_padded("Aggregated data"),
+      style = "padding-right: 0px;",
+      shinyWidgets::checkboxGroupButtons(
+        inputId = ns("amenity_density"),
+        choices = "Amenity Density",
+        justified = TRUE
       )
     )
   )
@@ -35,12 +44,18 @@ mod_layers_server <- function(id, point_layers, aggregate_layers) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    shiny::observeEvent(input$point_layers,
+    shiny::observeEvent(
+      {
+        input$apartment_buildings
+        input$apartment_evaluation
+      },
       ignoreInit = TRUE,
       ignoreNULL = FALSE,
       {
+        active_layers <- c(input$apartment_buildings, input$apartment_evaluation)
+
         # Update reactive with value from input
-        point_layers(input$point_layers)
+        point_layers(active_layers)
       }
     )
 
