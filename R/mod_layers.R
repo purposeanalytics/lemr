be95d853e7fc429dd5bc810f32f8bd0c9d548f5a
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-08-12 12:32:36 -0400

    Style legends, modal

--- R/mod_layers.R
+++ R/mod_layers.R
@@ -11,32 +11,46 @@ mod_layers_ui <- function(id) {
     width = 12,
     shiny::hr(),
     bigger_padded("Display additional layers"),
-    shiny::column(
-      width = 6,
-      # bigger_padded("Point data"),
-      style = "padding-left: 0px;",
-      shinyWidgets::checkboxGroupButtons(
-        inputId = ns("apartment_buildings"),
-        choices = list("Apartment buildings" = "apartment_buildings"),
-        justified = TRUE
+    shiny::fluidRow(
+      shiny::column(
+        width = 6,
+        shinyWidgets::checkboxGroupButtons(
+          inputId = ns("apartment_buildings"),
+          choices = list("Apartment buildings" = "apartment_buildings"),
+          justified = TRUE
+        )
       ),
-      shinyWidgets::checkboxGroupButtons(
-        inputId = ns("apartment_evaluation"),
-        choices = list("RentSafeTO Evaluation Scores" = "apartment_evaluation"),
-        justified = TRUE
-      ),
-      generate_layers_legend(c("#FFFFCC", "#FED976", "#FEB24C", "#FD8D3B", "#FC4E2B", "#BD0026", "#800126"), "Low", "100%")
+      shiny::column(
+        width = 6
+      )
     ),
-    shiny::column(
-      width = 6,
-      # bigger_padded("Aggregated data"),
-      style = "padding-right: 0px;",
-      shinyWidgets::checkboxGroupButtons(
-        inputId = ns("amenity_density"),
-        choices = list("Amenity Density" = "amenity_density"),
-        justified = TRUE
+    shiny::fluidRow(
+      shiny::column(
+        width = 6,
+        shinyWidgets::checkboxGroupButtons(
+          inputId = ns("apartment_evaluation"),
+          choices = list("RentSafeTO Evaluation Scores" = "apartment_evaluation"),
+          justified = TRUE
+        )
       ),
-      generate_layers_legend(c(low_colour, mid_colour, high_colour), "Low", "High")
+      shiny::column(
+        width = 6,
+        generate_layers_legend(c("#FFFFCC", "#FED976", "#FEB24C", "#FD8D3B", "#FC4E2B", "#BD0026", "#800126"), "Low", "100%")
+      )
+    ),
+    shiny::fluidRow(
+      shiny::column(
+        width = 6,
+        shinyWidgets::checkboxGroupButtons(
+          inputId = ns("amenity_density"),
+          choices = list("Amenity Density" = "amenity_density"),
+          justified = TRUE
+        )
+      ),
+      shiny::column(
+        width = 6,
+        generate_low_mid_high_legends(c(low_colour, mid_colour, high_colour), "Low", "Medium", "High")
+      )
     )
   )
 }
@@ -98,6 +112,31 @@ generate_layers_legend <- function(colors, min_text, max_text) {
           class = "colors",
           colors
         )
+      )
+    )
+  )
+}
+
+generate_low_mid_high_legends <- function(colors, min_text, mid_text, max_text) {
+  colors <- purrr::map(colors, function(x) {
+    shiny::div(class = "legend-element", style = glue::glue("background-color: {x};"))
+  })
+
+  shiny::div(
+    class = "legend",
+    shiny::div(
+      class = "triple-text",
+      shiny::tags$ul(
+        shiny::tags$li(min_text),
+        shiny::tags$li(mid_text),
+        shiny::tags$li(max_text)
+      )
+    ),
+    shiny::tags$li(
+      class = "legend-colors",
+      shiny::div(
+        class = "colors",
+        colors
       )
     )
   )
