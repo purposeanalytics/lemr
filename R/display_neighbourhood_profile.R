bf91b3c5227bf519687f211c72d06a3c5665f973
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-08-26 14:51:25 -0400

    Menu text instead of hamburger

--- R/display_neighbourhood_profile.R
+++ R/display_neighbourhood_profile.R
@@ -211,7 +211,7 @@ plot_amenity_density <- function(data, xaxis_title = FALSE, b = 15) {
   data <- data %>%
     dplyr::mutate(label = scales::percent(prop, accuracy = 0.1))
 
-  plot_ly(data,
+  plotly::plot_ly(data,
     x = ~group, y = ~prop, type = "bar", hoverinfo = "skip",
     marker = list(color = c(low_colour, mid_colour, high_colour)),
     text = ~label, textposition = "outside", cliponaxis = FALSE,
