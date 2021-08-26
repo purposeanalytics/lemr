a060bd3985d195dac09b9753ca86f3e7d350850e
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-08-26 09:37:33 -0400

    Move JS script to separate file

--- R/mod_search.R
+++ R/mod_search.R
@@ -12,49 +12,7 @@ mod_search_ui <- function(id) {
     shiny::column(
       width = 6,
       shiny::textInput(inputId = ns("address"), label = "Address", placeholder = "Search address..."),
-      shiny::HTML(paste0(" <script>
-                function initAutocomplete() {
-
-                var searchText = document.getElementById('search-address');
-
-                // Limit search to Toronto bounding box - not perfect, but should work?
-                var torontoBounds = new google.maps.LatLngBounds(
-                new google.maps.LatLng(43.58100, -79.63927),
-                new google.maps.LatLng(43.85547, -79.11525));
-
-                 var autocomplete = new google.maps.places.Autocomplete(searchText, {bounds: torontoBounds, types: ['geocode']});
-                // Limit to Canada
-                 autocomplete.setComponentRestrictions({'country': ['CA']});
-                 autocomplete.setFields(['address_components', 'formatted_address',  'geometry', 'icon', 'name']);
-                 autocomplete.addListener('place_changed', function() {
-                 var place = autocomplete.getPlace();
-                 if (!place.geometry) {
-                 return;
-                 }
-
-                 var addressPretty = place.formatted_address;
-                 var address = '';
-                 if (place.address_components) {
-                 address = [
-                 (place.address_components[0] && place.address_components[0].short_name || ''),
-                 (place.address_components[1] && place.address_components[1].short_name || ''),
-                 (place.address_components[2] && place.address_components[2].short_name || ''),
-                 (place.address_components[3] && place.address_components[3].short_name || ''),
-                 (place.address_components[4] && place.address_components[4].short_name || ''),
-                 (place.address_components[5] && place.address_components[5].short_name || ''),
-                 (place.address_components[6] && place.address_components[6].short_name || ''),
-                 (place.address_components[7] && place.address_components[7].short_name || '')
-                 ].join(' ');
-                 }
-                 var address_number =''
-                 address_number = [(place.address_components[0] && place.address_components[0].short_name || '')]
-                 var coords = place.geometry.location;
-                 //console.log(address);
-                 Shiny.onInputChange('search-jsValue', address);
-                 Shiny.onInputChange('search-jsValueAddressNumber', address_number);
-                 Shiny.onInputChange('search-jsValuePretty', addressPretty);
-                 Shiny.onInputChange('search-jsValueCoords', coords);});}
-                 </script>
+      shiny::HTML(paste0("
                  <script src='https://maps.googleapis.com/maps/api/js?key=", Sys.getenv("GOOGLE_CLOUD_TOKEN"), "&libraries=places&callback=initAutocomplete' async defer></script>"))
     ),
     shiny::column(
