d4d267e1d3e96870ad040f0bcbc35572d154bf39
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-09-17 12:03:26 -0400

    Remove building level evictions code

--- data-raw/points_layers/01-combine.R
+++ data-raw/points_layers/01-combine.R
@@ -91,37 +91,7 @@ buildings <- buildings %>%
   as_tibble() %>%
   left_join(agi_applications, by = "bing_address", suffix = c("_apt", "_agi"))
 
-# Add evictions hearings -----
-
-# eviction_hearings <- lemur::eviction_hearings %>%
-#   group_by(bing_address) %>%
-#   fill(landlord, .direction = "downup") %>%
-#   group_by(bing_address, neighbourhood, landlord) %>%
-#   summarise(
-#     geometry = geometry,
-#     hearings = sum(hearings),
-#     .groups = "drop"
-#   )
-#
-# eviction_hearings_coords <- eviction_hearings %>%
-#   group_by(bing_address) %>%
-#   slice(1) %>%
-#   ungroup() %>%
-#   st_coordinates() %>%
-#   as_tibble()
-#
-# eviction_hearings <- eviction_hearings %>%
-#   as_tibble() %>%
-#   select(-geometry) %>%
-#   distinct() %>%
-#   bind_cols(eviction_hearings_coords) %>%
-#   rename_at(vars(landlord, X, Y, neighbourhood), ~ paste0(.x, "_evictions"))
-#
-# buildings <- buildings %>%
-#   left_join(eviction_hearings %>%
-#     mutate(eviction_hearing = TRUE), by = "bing_address", suffix = c("_apt_agi", "_evictions"))
-
-# Fill in columns, prioritizing apt -> agi -> evictions
+# Fill in columns, prioritizing apt -> agi
 
 buildings <- buildings %>%
   mutate(
@@ -134,7 +104,6 @@ buildings <- buildings %>%
     tdf = coalesce(tdf, FALSE),
     tdf_year = na_if(tdf_year, ""),
     reduced_increase_by = na_if(reduced_increase_by, "")
-    # eviction_hearing = coalesce(eviction_hearing, FALSE)
   )
 
 # Select columns -----
