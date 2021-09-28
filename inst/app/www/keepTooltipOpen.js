2278132371591b4fca2792e7a436ef4eefd7230f
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-09-28 09:38:54 -0400

    JS for keeping tooltips open when they are hovered on, so we can put links etc in

---
+++ inst/app/www/keepTooltipOpen.js
@@ -0,0 +1,19 @@
+/* Source: https://embed.plnkr.co/plunk/HLqrJ6 */
+
+$( document ).ready(function() {
+  $(".fa-question-circle").popover({ trigger: "manual" , html: true, animation:false})
+  .on("mouseenter", function () {
+    var _this = this;
+    $(this).popover("show");
+    $(".popover").on("mouseleave", function () {
+      $(_this).popover('hide');
+    });
+  }).on("mouseleave", function () {
+    var _this = this;
+    setTimeout(function () {
+      if (!$(".popover:hover").length) {
+        $(_this).popover("hide");
+      }
+    }, 300);
+  });
+});
