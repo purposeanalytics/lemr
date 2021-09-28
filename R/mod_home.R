956f464ed9705f755629cf5a6cc47f9d9c7bb5c2
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-09-28 17:23:49 -0400

    CSS bits

--- R/mod_home.R
+++ R/mod_home.R
@@ -60,10 +60,11 @@ mod_home_ui <- function(id) {
             class = "vertical-align",
             shiny::column(
               width = 4,
-              shiny::icon("newspaper", class = "fa-5x")
+              shiny::icon("newspaper", class = "fa-5x fa-faw")
             ),
             shiny::column(
               width = 8,
+              class = "smaller",
               shiny::HTML("Read <b>Data Stories</b> above for our data analyses and learn how their implications may impact you, your organization, or your community.")
             )
           ),
@@ -72,10 +73,11 @@ mod_home_ui <- function(id) {
             class = "vertical-align",
             shiny::column(
               width = 4,
-              shiny::icon("layer-group", class = "fa-5x")
+              shiny::icon("layer-group", class = "fa-5x fa-faw")
             ),
             shiny::column(
               width = 8,
+              class = "smaller",
               shiny::p("Go to", shiny::tags$a(id = "link_map", href = "#", onclick = "link('Map')", shiny::tags$b("Map")), "for summary statistics, estimated rental supply, locations of awarded tenant defence fund grants, and more.")
             )
           ),
@@ -84,11 +86,12 @@ mod_home_ui <- function(id) {
             class = "vertical-align",
             shiny::column(
               width = 4,
-              shiny::icon("book-open", class = "fa-5x")
+              shiny::icon("book-open", class = "fa-5x fa-faw")
             ),
             shiny::column(
               width = 8,
-              shiny::p("Learn about the data sources included in the tool and find key terminology used throughout in", shiny::tags$a(id = "link_data_and_definitions", href = "#", onclick = "link('Data & Definitions')", shiny::tags$b("Data & Definitions")), ".")
+              class = "smaller",
+              shiny::p("Learn about the data sources included in the tool, find key terminology used throughout, and access processed data in", shiny::tags$a(id = "link_data_and_definitions", href = "#", onclick = "link('Data & Definitions')", shiny::tags$b("Data & Definitions")), ".")
             )
           )
         )
