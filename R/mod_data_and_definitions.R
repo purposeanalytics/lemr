6c75cbd51e4edcb157d93c3e5a3f107d8693b168
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-09-30 11:53:18 -0400

    Test out a few different layout downloads

--- R/mod_data_and_definitions.R
+++ R/mod_data_and_definitions.R
@@ -11,6 +11,95 @@ mod_data_and_definitions_ui <- function(id) {
     class = "content-page",
     id = "data-and-definitions-page",
     shiny::h1("Data & Definitions"),
+
+
+
+
+
+
+    shiny::fluidRow(
+      shiny::column(
+        width = 12,
+        shiny::p("The data used in the Low-end of Market Rental Monitor is available in two datasets and in a set of reports."),
+        shiny::downloadLink(ns("aggregate_layers"), label = shiny::tagList(shiny::h2("Low-end of Market Rental Monitor, Aggregate Layers", shiny::icon("download")))),
+          shiny::p("This dataset includes the data used in each of the aggregate layers in this tool, as well as all values shown in the full summary view, for each neighbourhood and the City of Toronto."),
+
+        shiny::downloadLink(ns("aggregate_layers"), label = shiny::tagList(shiny::h2("Low-end of Market Rental Monitor, Point Layers", shiny::icon("download")))),
+          shiny::p("This dataset includes the data used in each of the point layers in the tool. It contains all addresses and spatial information along with the layer values."),
+
+        shiny::downloadLink(ns("aggregate_layers"), label = shiny::tagList(shiny::h2("Low-end of Market Rental Monitor, Reports", shiny::icon("download")))),
+          shiny::p("These files include the Full Summary report for each neighbourhood and the City of Toronto, summarising data from the aggregate and point layers.")
+      )
+    ),
+
+
+
+    # shiny::fluidRow(
+    #   shiny::column(
+    #     width = 12,
+    #     shiny::p("The data used in the Low-end of Market Rental Monitor is available in two datasets and in a set of reports.")
+    #   ),
+    # ),
+    # shiny::fluidRow(
+    #   shiny::div(
+    #     class = "vertical-align",
+    #     shiny::column(
+    #       width = 9,
+    #       shiny::h2("Low-end of Market Rental Monitor, Aggregate Layers"),
+    #       shiny::p("This dataset includes the data used in each of the aggregate layers in this tool, as well as all values shown in the full summary view, for each neighbourhood and the City of Toronto.")
+    #     ),
+    #     shiny::column(
+    #       width = 3, align = "center",
+    #       shiny::downloadButton(
+    #         ns("aggregate_layers"),
+    #         shiny::HTML("<h3>Download<br/><i>Aggregate Layers</i></h3>"),
+    #         icon = NULL
+    #       )
+    #     )
+    #   )
+    # ),
+    # shiny::fluidRow(
+    #   shiny::div(
+    #     class = "vertical-align",
+    #     shiny::column(
+    #       width = 9,
+    #       shiny::h2("Low-end of Market Rental Monitor, Point Layers"),
+    #       shiny::p("This dataset includes the data used in each of the point layers in the tool. It contains all addresses and spatial information along with the layer values.")
+    #     ),
+    #     shiny::column(
+    #       width = 3, align = "center",
+    #       shiny::downloadButton(
+    #         ns("aggregate_layers"),
+    #         shiny::HTML("<h3>Download<br/><i>Point Layers</i></h3>"),
+    #         icon = NULL
+    #       )
+    #     )
+    #   )
+    # ),
+    # shiny::fluidRow(
+    #   shiny::div(
+    #     class = "vertical-align",
+    #     shiny::column(
+    #       width = 8,
+    #       shiny::h2("Low-end of Market Rental Monitor, Reports"),
+    #       shiny::p("These files include the Full Summary report for each neighbourhood and the City of Toronto, summarising data from the aggregate and point layers.")
+    #     ),
+    #     shiny::column(
+    #       width = 4, align = "center",
+    #       shiny::downloadButton(
+    #         ns("aggregate_layers"),
+    #         shiny::HTML("<h3>Download<br/><i>Reports</i></h3>"),
+    #         icon = NULL
+    #       )
+    #     )
+    #   )
+    # ),
+    shiny::div(class = "divider-line"),
+    shiny::tagList(
+      lemr::data_and_definitions %>%
+        dplyr::mutate(definition_full = purrr::pmap(list(.data$name, .data$description, .data$data_source_prefix, .data$data_source_suffix, .data$data_source, .data$data_source_link), format_definition)) %>%
+        dplyr::pull(.data$definition_full)
+    ),
     # I wish I could explain why this is necessary, but removing it makes the tooltips everywhere go away
     # I guess some different JS is getting attached. I don't know!
     shiny::div(
@@ -26,11 +115,6 @@ mod_data_and_definitions_ui <- function(id) {
           show = TRUE
         )
       )
-    ),
-    shiny::tagList(
-      lemr::data_and_definitions %>%
-        dplyr::mutate(definition_full = purrr::pmap(list(.data$name, .data$description, .data$data_source_prefix, .data$data_source_suffix, .data$data_source, .data$data_source_link), format_definition)) %>%
-        dplyr::pull(.data$definition_full)
     )
   )
 }
