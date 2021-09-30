8eb0687e8a50856e75faa3538677aab33c076294
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-09-30 12:38:08 -0400

    Don't ard code number of apartments

--- R/mod_data_story_agi_tdf.R
+++ R/mod_data_story_agi_tdf.R
@@ -5,8 +5,6 @@
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
 #' @noRd
-#'
-#' @importFrom shiny NS tagList
 mod_data_story_agi_tdf_ui <- function(id) {
   map_legend <- shiny::tagList(
     create_circle_legend(layer_colours[["agi_apartment"]], "Apartment buildings with AGI applications", alt_text = "A legend showing the colour of the points of above guideline increase applications for apartment buildings."),
@@ -30,7 +28,9 @@ mod_data_story_agi_tdf_ui <- function(id) {
         )
       ),
       shiny::p("A notice of rent increase, recalculating the monthly budget, cutting down on non-essential expenses - or finding a new home. The cycle is not unfamiliar to renters in Toronto. In Ontario, renters are protected by a provincially mandated rent increase guideline that is tied to the Consumer Price Index. However, when landlords incur capital expenses for completing major work, building security, or accessibility features, or extraordinary increases to their property tax, they may apply to pass those costs on to tenants through an Above Guideline Increase, or AGI. In turn, tenants may choose to contest an AGI through the Landlord Tenant Board, a process that can be time-consuming and costly. To support this, the City of Toronto offers assistance, financial and otherwise, through the Tenant Defense Fund (TDF) program to tenants in private rental housing."),
-      shiny::p("In the City of Toronto, AGI applications are abundant. Out of 3,480 apartment buildings in the city, a quarter of them had at least one AGI application in the last five years, and of these, just under a quarter received a TDF grant. In this story, we look at the distribution of AGIs and TDF grants across neighbourhoods of Toronto."),
+      shiny::p(glue::glue("In the City of Toronto, AGI applications are abundant. Out of {n_apt} privately owned apartment buildings in the city, almost thirty percent of them had at least one AGI application in the last five years, and of these, just under a quarter received a TDF grant. In this story, we look at the distribution of AGIs and TDF grants across neighbourhoods of Toronto.",
+        n_apt = scales::comma(city_aggregate[["number_of_buildings_private"]])
+      )),
       shiny::fluidRow(
         shiny::column(
           width = 12,
