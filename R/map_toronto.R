6b6211332020b3680828447f953aacb86c77864d
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-10-01 13:58:31 -0400

    Remove compass from map

--- R/map_toronto.R
+++ R/map_toronto.R
@@ -10,6 +10,6 @@ map_toronto <- function() {
     mapboxer::as_mapbox_source() %>%
     mapboxer::mapboxer(style = "mapbox://styles/purposeanalytics/cksw80iwn155y1anq5l1f0v5n", center = c(-79.3753, 43.7173), zoom = 12, pitch = 0, bearing = bearing, minZoom = 8) %>%
     mapboxer::fit_bounds(sf::st_bbox(lemr::toronto), pitch = 0, bearing = bearing) %>%
-    mapboxer::add_navigation_control() %>%
+    mapboxer::add_navigation_control(showCompass = FALSE) %>%
     mapboxer::add_line_layer(line_color = default_line_colour, line_width = 1.5)
 }
